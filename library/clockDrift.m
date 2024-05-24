f70fb2da06173d6257c096a108de537174eed98e
VictoriaHurd <85904750+victoria-hurd@users.noreply.github.com>
2024-05-23 18:35:08 -0600

    Update clockDrift.m

--- library/clockDrift.m
+++ library/clockDrift.m
@@ -7,8 +7,7 @@
 %           chooseID: inputted IDs to analyze
 % OUTPUTS: 
 
-% THOUGHTS FOR TOMORROW: calculate mean for every second. Calculate std for
-% every second as well. Plot mean as a line. Plot std as well. Threshold
+% THOUGHTS FOR TOMORROW: Plot mean as a line. Plot std as well. Threshold
 % for when acceleration goes up by XYZ value (for now, 0.1G, spin up rate?)
 % Plot for each trial
 % Plot for each sensor
@@ -52,19 +51,19 @@ chooseID = "01-01";
     % Variables to plot against - truth acceleration and truth time
     truthAcc = Ti.magnitude_Centrifuge;
     t = Ti.Time_s_;
-    % First plot Tobii acceleration
-    tobiiAcc = Ti.magnitude_Tobii;
+    % First plot experimental acceleration
+    expAcc = Ti.magnitude_Tobii;
 
     % Normalize variables
     ZtruthAcc = (truthAcc - min(truthAcc))/(max(truthAcc)-min(truthAcc));
-    tobiiAcc = (tobiiAcc - min(tobiiAcc))/(max(tobiiAcc)-min(tobiiAcc));
+    expAcc = (expAcc - min(expAcc))/(max(expAcc)-min(expAcc));
 
     % Smooth variables with Savitz-Golay to remove noise
     framelen = 51;
     truthFilt = sgolayfilt(ZtruthAcc, 3, framelen);
     % remove baseline Tobii noise?
-    tobiiAcc = tobiiAcc-mean(tobiiAcc(1:100));
-    tobiiAccFilt = sgolayfilt(tobiiAcc, 3, framelen);
+    expAcc = expAcc-mean(expAcc(1:100));
+    expAccFilt = sgolayfilt(expAcc, 3, framelen);
 
     % Calculate Moving Average Mean 
     % 1 second averaging and std calculation
@@ -72,40 +71,47 @@ chooseID = "01-01";
     tWindow = 1; % [sec]
     % Find number of samples that represents window time
     N = find(t==tWindow);
-    % Define averaged acceleration vector
-    
-    % loop through entire time vector
-    for j = 1:length(t)
-        
-    end
 
-    % TBD
+    % Instead of looping through, make more efficient by reshaping and
+    % taking mean and std of the columns
+    % First, if length isn't a multiple of N, chop off the end. We
+    % will cut off the end of the data since end of data doesn't matter due
+    % to its occurrence after the last validated acceleration correlation
+    % point
+    shortExpAcc = expAccFilt(1:end-rem(length(expAccFilt),N));
+    shortTruthAcc = truthFilt(1:end-rem(length(expAccFilt),N));
+    shortt = t(1:end-rem(length(expAccFilt),N));
+
+    % Now reshape into a matrix of N rows by (length of short vector)/N
+    matrixExpAcc = reshape(shortExpAcc,N,[]);
+    matrixTruthAcc = reshape(shortTruthAcc,N,[]);
+ 
+    % Calculate mean and std for each reshaped matrix
+    avgExpAcc = mean(matrixExpAcc);
+    stdExpAcc = std(matrixExpAcc);
+    avgTruthAcc = mean(matrixTruthAcc);
+    stdTruthAcc = std(matrixTruthAcc);
+
+     % Keep every N values of t
+    plott = linspace(min(shortt),max(shortt),length(avgExpAcc));
 
     % Identify when acceleration goes above threshold value
     % TBD
 
-
-x = 1:100;
-y = 1:100;
-tobiiSlope = zeros(length(t)-1,1);
-k = 5;
-testY = movmean(tobiiAcc,k);
-testX = t;
-testSlope = gradient(testY);
-
-% Plotting results
+%% Plotting results
     figure
     hold on 
-    title("Tobii vs Centrifuge Acceleration Comparison - Normalized, Baseline Removed, Savitz-Golay Filtered")
+    title("Tobii vs Centrifuge Acceleration Comparison")
     xlabel("Aligned Time [s]")
     ylabel("Measured Acceleration [G]")
-    plot(t,tobiiAccFilt)
+    plot(t,expAccFilt)
     plot(t,truthFilt)
-    plot(t, testSlope)
+    errorbar(plott, avgExpAcc,stdExpAcc)
+    errorbar(plott, avgTruthAcc,stdTruthAcc)
     xline(GORstart, '-r','LineWidth',2)
     xline(RORstart, '-b','LineWidth',2)
-    legend("Tobii Magnitude Acceleration","Centrifuge Ground Truth","Slope",...
-        "GOR Start","ROR Start","Location","northwest")
+    legend("Tobii Acceleration","Centrifuge Ground Truth","Averaged Tobii Acceleration",...
+        "Averaged Truth Acceleration","GOR Start","ROR Start","Location","northwest")
     hold off
 
 %end
