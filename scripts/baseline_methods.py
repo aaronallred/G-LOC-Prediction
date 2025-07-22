updated baseline functions to fix slotting of derivatives

scripts/baseline_methods.py
@@ -127,7 +127,25 @@ def create_v1_baseline(baseline_window, trial_column, time_column, features_phys
         time_array = np.array(time_column)
 
         # Find baseline average based on specified baseline window
-        baseline_feature = np.mean(features_phys[index_window], axis = 0)
+        # baseline_feature = np.mean(features_phys[index_window], axis = 0)
+        #
+        # # Ensure that data doesn't introduce NaNs.
+        # if baseline_feature.shape[0] == 0:
+        #     # If baseline window is empty, use ones
+        #     baseline_feature = np.ones(features_phys.shape[1])
+        # baseline_feature = np.nan_to_num(baseline_feature,nan=1)
+        # baseline_feature = np.where(baseline_feature == 0, 1, baseline_feature)
+
+        # Find baseline average based on specified baseline window
+        # Ensure that data doesn't introduce NaNs.
+        if np.sum(index_window) == 0:
+            # Empty baseline window — use ones
+            baseline_feature = np.ones(features_phys.shape[1])
+        else:
+            # Compute mean baseline
+            baseline_feature = np.mean(features_phys[index_window], axis=0)
+            baseline_feature = np.nan_to_num(baseline_feature, nan=1)
+            baseline_feature = np.where(baseline_feature == 0, 1, baseline_feature)
 
         # Divide features for that trial by baselined data
         baseline_v1[trial_id_in_data[i]] = np.array(features_phys[(trial_column == trial_id_in_data[i])] /baseline_feature)
@@ -170,6 +188,7 @@ def create_v2_baseline(baseline_window, trial_column, time_column, features_phys
 
         # Find baseline average based on specified baseline window
         baseline_feature = np.mean(features_phys[index_window], axis = 0)
+        baseline_feature = np.nan_to_num(baseline_feature, nan=0)
 
         # Subtract baseline data (baseline v2)
         baseline_v2[trial_id_in_data[i]] = np.array(features_phys[(trial_column == trial_id_in_data[i])] - baseline_feature)
@@ -351,6 +370,7 @@ def create_v5_baseline(baseline_window, trial_column, time_column, subject_colum
         # Find baseline for current participant
         current_participant = participant_array[time_index][0]
         baseline_feature = participant_seated_rhr[current_participant]
+        baseline_feature = np.nan_to_num(baseline_feature, nan=1)
 
         # Divide features for that trial by baselined data
         baseline_v5[trial_id_in_data[i]] = np.array(features_ecg[(trial_column == trial_id_in_data[i])] /baseline_feature)
@@ -394,6 +414,7 @@ def create_v6_baseline(baseline_window, trial_column, time_column, subject_colum
         # Find baseline for current participant
         current_participant = participant_array[time_index][0]
         baseline_feature = participant_seated_rhr[current_participant]
+        baseline_feature = np.nan_to_num(baseline_feature, nan=0)
 
         # Divide features for that trial by baselined data
         baseline_v6[trial_id_in_data[i]] = np.array(features_ecg[(trial_column == trial_id_in_data[i])] - baseline_feature)
@@ -608,19 +629,15 @@ def combine_all_baseline(trial_column, baseline, baseline_derivative, baseline_s
 
     # Iterate through all unique trial_id & combine the baseline, baseline derivative, and baseline second derivative
     for trial in trial_id_in_data:
-        # all_baseline_data = []
-        for i, method in enumerate(baseline.keys()):
-            m,n = baseline[method][trial].shape
-            start = i * n
-            end = start + n
-            combined_baseline[trial][:,start:end] = baseline[method][trial].astype(np.float32)
-            combined_baseline[trial][:,start+n:end+n] = baseline_derivative[method][trial].astype(np.float32)
-            combined_baseline[trial][:,start+2*n:end+2*n] = baseline_second_derivative[method][trial].astype(np.float32)
-            # all_baseline_data.append(baseline[method][trial])
-            # all_baseline_data.append(baseline_derivative[method][trial])
-            # all_baseline_data.append(baseline_second_derivative[method][trial])
+        all_data = []
+        for method in baseline.keys():
+            base = baseline[method][trial].astype(np.float32)
+            deriv = baseline_derivative[method][trial].astype(np.float32)
+            second = baseline_second_derivative[method][trial].astype(np.float32)
+            all_data.append(np.hstack([base, deriv, second]))
 
-        # combined_baseline2[trial] = np.column_stack(tuple(all_baseline_data))
+        combined_baseline[trial] = np.hstack(all_data).astype(np.float32)
+
 
     combined_baseline_names = sum([baseline_names[method] + [s + '_derivative' for s in baseline_names[method]] +
                                        [s + '_2derivative' for s in baseline_names[method]] for method in baseline_names.keys()], [])
@@ -785,3 +802,40 @@ def baseline_data_old(baseline_methods_to_use, gloc_data_reduced, features,time_
     baseline_names_v0 = baseline_names['v0']
 
     return combined_baseline, combined_baseline_names, baseline_v0, baseline_names_v0
+
+def combine_all_baseline_old(trial_column, baseline, baseline_derivative, baseline_second_derivative, baseline_names):
+    """
+    This function combines the features, derivative of features, and second derivative of features into one np array.
+    """
+
+    # Find Unique Trial ID
+    trial_id_in_data = trial_column.unique()
+
+    # Preallocate the dictionary with NumPy arrays
+    num_cols = 0
+    for method in baseline.keys():
+        num_cols += baseline[method][trial_id_in_data[0]].shape[1]*3
+    combined_baseline = {trial: np.empty((baseline[list(baseline.keys())[0]][trial].shape[0], num_cols), dtype=np.float32) for trial in trial_id_in_data}
+    # combined_baseline2 = {trial: np.empty((baseline[list(baseline.keys())[0]][trial].shape[0], 0)) for trial in
+    #                      trial_id_in_data}
+
+    # Iterate through all unique trial_id & combine the baseline, baseline derivative, and baseline second derivative
+    for trial in trial_id_in_data:
+        # all_baseline_data = []
+        for i, method in enumerate(baseline.keys()):
+            m,n = baseline[method][trial].shape
+            start = i * n
+            end = start + n
+            combined_baseline[trial][:,start:end] = baseline[method][trial].astype(np.float32)
+            combined_baseline[trial][:,start+n:end+n] = baseline_derivative[method][trial].astype(np.float32)
+            combined_baseline[trial][:,start+2*n:end+2*n] = baseline_second_derivative[method][trial].astype(np.float32)
+            # all_baseline_data.append(baseline[method][trial])
+            # all_baseline_data.append(baseline_derivative[method][trial])
+            # all_baseline_data.append(baseline_second_derivative[method][trial])
+
+        # combined_baseline2[trial] = np.column_stack(tuple(all_baseline_data))
+
+    combined_baseline_names = sum([baseline_names[method] + [s + '_derivative' for s in baseline_names[method]] +
+                                       [s + '_2derivative' for s in baseline_names[method]] for method in baseline_names.keys()], [])
+
+    return combined_baseline, combined_baseline_names