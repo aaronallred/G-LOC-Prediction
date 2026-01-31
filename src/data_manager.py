eaa685c17a121a6a00382004a9da6aac02e1fbb5
jaspershen21 <jaspershen21@gmail.com>
2026-01-31 12:06:52 -0700

    Refactor function to get data locations

--- src/data_manager.py
+++ src/data_manager.py
@@ -1,16 +1,14 @@
 import pandas as pd
 import numpy as np
+import os
 from sklearn.model_selection import StratifiedKFold, KFold
 
 class DataManager:
-    IMPLICIT_FEATURE_GROUPS = {"ECG", "BR", "temp", "eyetracking", "rawEEG"} # All physiological signals
-    EXPLICIT_FEATURE_GROUPS = IMPLICIT_FEATURE_GROUPS.union({"AFE", "G", "processedEEG", "demographics", "strain"}) # All physiological and participant info
-    COMPLETE_FEATURE_GROUPS = {"AFE"} # Includes nonAFE and AFE trials
-
-    def __init__(self, file_path):
-        self.file_path = file_path
+    def __init__(self, data_path = "../data/"):
+        self.data_path = data_path
 
     def get_data(
+            self,
             model_type,
             num_splits,
             kfold_ID,
@@ -52,34 +50,144 @@ class DataManager:
                     *Additional Note: Not shifted by horizon for advanced classifiers (happens in the data loader inside)
                 all_features    --> List of all feature names in x_train and x_test
         """
-        # Determining features to use for the dataset
+        ################################################### FEATURES SETUP ###################################################
+        # Feature Groups to Analyze
+        IMPLICIT_FEATURE_GROUPS = {"ECG", "BR", "temp", "eyetracking", "rawEEG"} # All physiological signals
+        EXPLICIT_FEATURE_GROUPS = IMPLICIT_FEATURE_GROUPS.union({"AFE", "G", "processedEEG", "demographics", "strain"}) # All physiological and participant info
+        COMPLETE_FEATURE_GROUPS = {"AFE"} # Include nonAFE and AFE trials
+        
+        # TODO: Change model type to an ENUM or similar structure for clarity
         feature_groups_to_analyze = set()
         if model_type[0] == "Complete":
-            feature_groups_to_analyze = DataManager.COMPLETE_FEATURE_GROUPS
+            feature_groups_to_analyze = COMPLETE_FEATURE_GROUPS
         # noAFE restriction is imposed later when filtering out rows
 
         if model_type[1] == "Implicit":
-            feature_groups_to_analyze = feature_groups_to_analyze.union(DataManager.IMPLICIT_FEATURE_GROUPS)
+            feature_groups_to_analyze = feature_groups_to_analyze.union(IMPLICIT_FEATURE_GROUPS)
         elif model_type[1] == "Explicit":
-            feature_groups_to_analyze = feature_groups_to_analyze.union(DataManager.EXPLICIT_FEATURE_GROUPS)
+            feature_groups_to_analyze = feature_groups_to_analyze.union(EXPLICIT_FEATURE_GROUPS)
 
         # NOTE:
         # AFE indicator is required for EEG imputation in complete models,
         # but is only included as a predictive feature for explicit models.
 
-        # Set baseline characteristics. Depends on model type
+        # Baselining Characteristics
         if model_type[0] == "noAFE":
             baseline_methods_to_use = ["v0", "v1", "v2", "v5", "v6", "v7", "v8"]
         else:
             baseline_methods_to_use = ["v0", "v1", "v2", "v5", "v6"]
 
-    def get_ml_splitter(self, n_splits=5):
-        return StratifiedKFold(n_splits=n_splits)
 
-    def get_dl_splitter(self, n_splits=5):
-        # Maybe DL uses simple KFold or specific time-series split
-        return KFold(n_splits=n_splits)
 
-    def save_split_indices(self, train_idx, val_idx, path):
-        # Save indices to recreate the split later
-        pass
+        ############################################# LOAD AND PROCESS DATA #############################################
+        file_paths = self.get_data_locations()
+
+        
+
+    def get_data_locations(self):
+        """
+            Get the file locations for all relevant data files
+            
+            Parameters:
+                datafolder (str) --> location of AFRL provided data from the experiment: raw data that is processed
+
+            Returns:
+                dict: A dictionary containing file paths for the data with the following structure:
+                - "main": Path to the main data CSV file.
+                - "baseline": Path to the baseline data CSV file.
+                - "demographic": Path to the demographic data CSV file.
+                - "eeg_list": List of paths to individual EEG data files.
+                - "baseline_eeg_processed_list": List of paths to baseline EEG processed data files.   
+        """
+        # Data CSV
+        main_data_file_path = os.path.join(self.data_path, "all_trials_25_hz_stacked_null_str_filled.csv")
+
+        # Baseline Data (HR)
+        baseline_data_file_path = os.path.join(self.data_path, "ParticipantBaseline.csv")
+
+        # Modified Demographic Data (put in order of participant 1-13, removed excess calculations, and converted from .xlsx to .csv)
+        demographic_data_file_path = os.path.join(self.data_path, "GLOC_Effectiveness_Final.csv")
+
+        # Input GOR EEG data from separate files
+        list_of_eeg_data_file_paths = [
+            os.path.join(self.data_path, "GLOC_GOR_EEG_data_participants_1-13/GLOC_01_DC1_25Hz_EEG_power_wMAR.xlsx"),
+            os.path.join(self.data_path, "GLOC_GOR_EEG_data_participants_1-13/GLOC_01_DC2_25Hz_EEG_power_wMAR.xlsx"),
+            os.path.join(self.data_path, "GLOC_GOR_EEG_data_participants_1-13/GLOC_01_DC3_25Hz_EEG_power_wMAR.xlsx"),
+            os.path.join(self.data_path, "GLOC_GOR_EEG_data_participants_1-13/GLOC_02_DC1_25Hz_EEG_power_wMAR.xlsx"),
+            os.path.join(self.data_path, "GLOC_GOR_EEG_data_participants_1-13/GLOC_02_DC2_25Hz_EEG_power_wMAR.xlsx"),
+            os.path.join(self.data_path, "GLOC_GOR_EEG_data_participants_1-13/GLOC_02_DC3_25Hz_EEG_power_wMAR.xlsx"),
+            os.path.join(self.data_path, "GLOC_GOR_EEG_data_participants_1-13/GLOC_03_DC1_25Hz_EEG_power_wMAR.xlsx"),
+            os.path.join(self.data_path, "GLOC_GOR_EEG_data_participants_1-13/GLOC_03_DC2_25Hz_EEG_power_wMAR.xlsx"),
+            os.path.join(self.data_path, "GLOC_GOR_EEG_data_participants_1-13/GLOC_03_DC3_25Hz_EEG_power_wMAR.xlsx"),
+            os.path.join(self.data_path, "GLOC_GOR_EEG_data_participants_1-13/GLOC_04_DC1_25Hz_EEG_power_wMAR.xlsx"),
+            os.path.join(self.data_path, "GLOC_GOR_EEG_data_participants_1-13/GLOC_04_DC2_25Hz_EEG_power_wMAR.xlsx"),
+            os.path.join(self.data_path, "GLOC_GOR_EEG_data_participants_1-13/GLOC_04_DC3_25Hz_EEG_power_wMAR.xlsx"),
+            os.path.join(self.data_path, "GLOC_GOR_EEG_data_participants_1-13/GLOC_05_DC1_25Hz_EEG_power_wMAR.xlsx"),
+            os.path.join(self.data_path, "GLOC_GOR_EEG_data_participants_1-13/GLOC_05_DC2_25Hz_EEG_power_wMAR.xlsx"),
+            os.path.join(self.data_path, "GLOC_GOR_EEG_data_participants_1-13/GLOC_05_DC3_25Hz_EEG_power_wMAR.xlsx"),
+            os.path.join(self.data_path, "GLOC_GOR_EEG_data_participants_1-13/GLOC_06_DC1_25Hz_EEG_power_wMAR.xlsx"),
+            os.path.join(self.data_path, "GLOC_GOR_EEG_data_participants_1-13/GLOC_06_DC4_25Hz_EEG_power_wMAR.xlsx"),
+            os.path.join(self.data_path, "GLOC_GOR_EEG_data_participants_1-13/GLOC_06_DC6_25Hz_EEG_power_wMAR.xlsx"),
+            os.path.join(self.data_path, "GLOC_GOR_EEG_data_participants_1-13/GLOC_07_DC2_25Hz_EEG_power_wMAR.xlsx"),
+            os.path.join(self.data_path, "GLOC_GOR_EEG_data_participants_1-13/GLOC_07_DC4_25Hz_EEG_power_wMAR.xlsx"),
+            os.path.join(self.data_path, "GLOC_GOR_EEG_data_participants_1-13/GLOC_07_DC6_25Hz_EEG_power_wMAR.xlsx"),
+            os.path.join(self.data_path, "GLOC_GOR_EEG_data_participants_1-13/GLOC_08_DC1_25Hz_EEG_power_wMAR.xlsx"),
+            os.path.join(self.data_path, "GLOC_GOR_EEG_data_participants_1-13/GLOC_08_DC3_25Hz_EEG_power_wMAR.xlsx"),
+            os.path.join(self.data_path, "GLOC_GOR_EEG_data_participants_1-13/GLOC_09_DC2_25Hz_EEG_power_wMAR.xlsx"),
+            os.path.join(self.data_path, "GLOC_GOR_EEG_data_participants_1-13/GLOC_09_DC5_25Hz_EEG_power_wMAR.xlsx"),
+            os.path.join(self.data_path, "GLOC_GOR_EEG_data_participants_1-13/GLOC_09_DC6_25Hz_EEG_power_wMAR.xlsx"),
+            os.path.join(self.data_path, "GLOC_GOR_EEG_data_participants_1-13/GLOC_10_DC2_25Hz_EEG_power_wMAR.xlsx"),
+            os.path.join(self.data_path, "GLOC_GOR_EEG_data_participants_1-13/GLOC_10_DC4_25Hz_EEG_power_wMAR.xlsx"),
+            os.path.join(self.data_path, "GLOC_GOR_EEG_data_participants_1-13/GLOC_10_DC5_25Hz_EEG_power_wMAR.xlsx"),
+            os.path.join(self.data_path, "GLOC_GOR_EEG_data_participants_1-13/GLOC_11_DC1_25Hz_EEG_power_wMAR.xlsx"),
+            os.path.join(self.data_path, "GLOC_GOR_EEG_data_participants_1-13/GLOC_12_DC1_25Hz_EEG_power_wMAR.xlsx"),
+            os.path.join(self.data_path, "GLOC_GOR_EEG_data_participants_1-13/GLOC_12_DC5_25Hz_EEG_power_wMAR.xlsx"),
+            os.path.join(self.data_path, "GLOC_GOR_EEG_data_participants_1-13/GLOC_13_DC1_25Hz_EEG_power_wMAR.xlsx"),
+            os.path.join(self.data_path, "GLOC_GOR_EEG_data_participants_1-13/GLOC_13_DC3_25Hz_EEG_power_wMAR.xlsx"),
+            os.path.join(self.data_path, "GLOC_GOR_EEG_data_participants_1-13/GLOC_13_DC6_25Hz_EEG_power_wMAR.xlsx")
+        ]
+
+        # Input baseline EEG data from separate files
+        list_of_baseline_eeg_processed_file_paths = [
+            os.path.join(self.data_path, "GLOC_EEG_baseline_delta_noAFE1.csv"),
+            os.path.join(self.data_path, "GLOC_EEG_baseline_theta_noAFE1.csv"),
+            os.path.join(self.data_path, "GLOC_EEG_baseline_alpha_noAFE1.csv"),
+            os.path.join(self.data_path, "GLOC_EEG_baseline_beta_noAFE1.csv")
+        ]
+
+        file_paths = {
+            "main": main_data_file_path,
+            "baseline": baseline_data_file_path,
+            "demographic": demographic_data_file_path,
+            "eeg_list": list_of_eeg_data_file_paths,
+            "baseline_eeg_processed_list": list_of_baseline_eeg_processed_file_paths
+        }
+
+        return file_paths
+    
+# TODO: Change analysis type to an ENUM
+def process_csv_by_analysis_type(analysis_type, filename, feature_groups_to_analyze, demographic_data_filename,
+                                   model_type, list_of_eeg_data_files, trial_to_analyze, subject_to_analyze):
+    if analysis_type == 0:  # One Trial / One Subject
+        (gloc_data_reduced, features, features_phys, features_ecg, features_eeg, all_features, all_features_phys,
+         all_features_ecg, all_features_eeg) = (load_and_process_csv(filename, analysis_type, feature_groups_to_analyze,
+                                                                     demographic_data_filename, model_type,
+                                                                     list_of_eeg_data_files,
+                                                                     trial_to_analyze=trial_to_analyze,
+                                                                     subject_to_analyze=subject_to_analyze))
+
+    elif analysis_type == 1:  # All Trials for One Subject
+        (gloc_data_reduced, features, features_phys, features_ecg, features_eeg, all_features, all_features_phys,
+         all_features_ecg, all_features_eeg) = (load_and_process_csv(filename, analysis_type, feature_groups_to_analyze,
+                                                                     demographic_data_filename, model_type,
+                                                                     list_of_eeg_data_files,
+                                                                     subject_to_analyze=subject_to_analyze))
+
+    else:  # All Trials for All Subjects
+        (gloc_data_reduced, features, features_phys, features_ecg, features_eeg, all_features, all_features_phys,
+         all_features_ecg, all_features_eeg) = (load_and_process_csv(filename, analysis_type, feature_groups_to_analyze,
+                                                                     demographic_data_filename, model_type,
+                                                                     list_of_eeg_data_files))
+
+    return (gloc_data_reduced, features, features_phys, features_ecg, features_eeg, all_features, all_features_phys,
+            all_features_ecg, all_features_eeg)
