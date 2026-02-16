6ba91b42a6bcfc792017596518950a73a6910b66
jaspershen21 <jaspershen21@gmail.com>
2026-02-15 22:40:41 -0700

    Add remove NaN trials functionality

--- src/data_manager.py
+++ src/data_manager.py
@@ -75,6 +75,13 @@ class DataManager:
         if model_type[0] != "Complete":
             self._eeg_specific_imputation(gloc_data, features)
 
+        ############################################### MISSING DATA HANDLING ###############################################
+        """
+        Optional handling of raw NaN data, depending on 'remove_NaN_trials' 'impute_type' <= 1
+        """
+        if remove_NaN_trials:
+            nan_proportion_df = self._remove_all_nan_trials(gloc_data, features, gloc_labels)
+
     def _get_feature_groups_and_baseline_methods(self, model_type):
         FEATURE_GROUPS_BY_MODEL_TYPE = {
             ("noAFE", "Explicit"): {"ECG", "BR", "temp", "eyetracking", "rawEEG", "AFE", "G", "processedEEG", "demographics", "strain"},
@@ -433,3 +440,65 @@ class DataManager:
             if verbose:
                 for col, n in missing_counts.items():
                     print(f"Imputed {n} values in '{col}' for AFE rows")
+
+    def _remove_all_nan_trials(self, gloc_data, features, gloc_labels, verbose = True):
+        """
+            Remove trials where there is at least one data stream that is all NaN
+            Also returns a NaN proportionality table that says for each trial, what prop are NaN for each data stream
+        """
+        # All features and subject trial info to be put into a reduced dataframe from gloc_data
+        all_features_with_ids = features["All"] + ["subject", "trial"]
+        reduced_data_frame = gloc_data[all_features_with_ids]
+
+        rows_to_remove = []
+        nan_proportion_table = []
+
+        N = 0 # number of trials total
+        M = 0 # number of trials with missing data streams
+        for (subject, trial), group in reduced_data_frame.groupby(["subject", "trial"], sort = False):
+            feature_block = group[features["All"]]
+
+            # Compute proportion of NaN values for each feature
+            nan_proportions = feature_block.isna().mean().to_dict()
+
+            # Store subject-trial and NaN proportions
+            nan_proportion_table.append({"subject-trial": f"{subject}-{trial}", **nan_proportions})
+
+            # Check if any of the columns in the trial data are entirely NaN
+            all_nan_cols = feature_block.isna().all()
+            if all_nan_cols.any():
+                # If so, add these indices to the list of rows to remove
+                rows_to_remove.append(group.index)
+
+                # Identify feature names that are completely NaN
+                nan_features = all_nan_cols[all_nan_cols].index.tolist()
+
+                # Print info about which trial and features were missing if verbose is true (default is false / no print)
+                if verbose:
+                    print(f"Subject {subject}, Trial {trial}: features entirely NaN → {nan_features}")
+
+                M += 1 # count missing trials
+
+            N += 1 # count trials
+
+        # Flatten list of indices and remove them from the DataFrame
+        rows_to_remove = [item for sublist in rows_to_remove for item in sublist]
+
+        # Convert from a dict to a DF
+        nan_proportion_df = pd.DataFrame(nan_proportion_table)
+
+        # Get rid of rows in the DF and array (in place)
+        keep_mask = np.ones(len(gloc_data), dtype=bool)
+        keep_mask[rows_to_remove] = False
+
+        gloc_data.drop(rows_to_remove, inplace = True)
+        gloc_data.reset_index(drop = True, inplace = True)
+
+        kept_labels = gloc_labels[keep_mask]
+        gloc_labels.resize(kept_labels.shape, refcheck = False)
+        gloc_labels[:] = kept_labels
+
+        # Print NaN findings
+        print(f"There are {M} trials with all NaNs for at least one feature out of {N} trials. {N - M} trials remaining.")
+
+        return nan_proportion_df
