0568b37f012b6edac93b90468432f74431a512e2
jaspershen21 <jaspershen21@gmail.com>
2026-04-14 00:29:20 -0600

    Add method to get subfolder from model type

--- src/model_type.py
+++ src/model_type.py
@@ -4,3 +4,6 @@ from dataclasses import dataclass
 class ModelType:
     afe_filter: str  # "noAFE" or "Complete"
     feature_set: str # "Explicit" or "Implicit"
+
+    def get_folder_name(self) -> str:
+        return f"{self.afe_filter}_{self.feature_set}"
