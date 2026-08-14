32a68b957a8d644ab7cebcc89fb75acd6812481a
jaspershen21 <jaspershen21@gmail.com>
2026-08-13 20:16:00 -0600

    Encforce numpy data type in faster_knn_impute

--- src/scripts/imputation_traditional.py
+++ src/scripts/imputation_traditional.py
@@ -149,6 +149,7 @@ def faster_knn_impute(X, k=5, M=32, efSearch=64):
   Returns:
   - X_imputed: Matrix with missing values imputed
   """
+  X = np.asarray(X, dtype = np.float64)
   mask = np.isnan(X)
   X_imputed = X.copy()
   # Temporarily mean impute missing values
